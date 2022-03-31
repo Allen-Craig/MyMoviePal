fc21d8133c18747767eafcf6180284ed03ac08e8
Allen Craig <TheMightyCraig@gmail.com>
2022-03-30 20:08:49 -0700

    added divs and moved text

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,44 +5,50 @@ export function SignUp() {
   return (
     <div>
       {" "}
-      <h1>MyMoviePal</h1>
+      <div className="head">
+        <h1>MyMoviePal</h1>
+        <p>
+          Sign up by entering in your User Name, Email, and creating a Password
+          to see all of your favorite movies!
+        </p>
+      </div>
       <form>
         {" "}
-        <label for="sign-up-user-name">
-          <b>User Name</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter User Name"
-          id="sign-up-user-name"
-          name="sign-up-user-name"
-        />
-        <label for="sign-up-password">
-          <b>Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          id="sign-up-password"
-          name="sign-up-password"
-        />
-        <label for="sign-up-user-email">
-          <b>User Email</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Your Email"
-          id="sign-up-user-email"
-          name="sign-up-user-email"
-        />
-        <button>Sign Up</button>
-      </form>
-      <div class="container">
-        <div class="vertical-center">
-          <button className="home-button" onClick={() => navigate("/")}>
-            Back to Home Page
-          </button>
+        <div class="container">
+          <label for="sign-in-user-name">
+            <b>User Name</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter User Name"
+            id="sign-up-user-name"
+            name="sign-up-user-name"
+          />
+          <label for="sign-up-password">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            id="sign-up-password"
+            name="sign-up-password"
+          />
+          <label for="sign-up-user-email">
+            <b>User Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Your Email"
+            id="sign-up-user-email"
+            name="sign-up-user-email"
+          />
+          <button>Sign Up</button>
         </div>
+      </form>
+      <div class="button2">
+        <button className="home-button" onClick={() => navigate("/")}>
+          <b>Back to Home Page</b>
+        </button>
       </div>
     </div>
   );
